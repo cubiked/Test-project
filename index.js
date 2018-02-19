abe08f835c2b8269a7d0140da0288749c2fee9f4
Edward Carey <ed@jigsaw.xyz>
2018-02-19 14:09:37 +0000

    working Express app with dishes, leaders and promotions routes

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
+
 const express = require('express');
 const http = require('http');
+
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 //importing the dishRouter code (remember './' is used when calling a file based node module)
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
 const hostname = 'localhost';
 const port = 3000;
 
-//the below const is saying that the application is going to be using the express node module.  Express then provides methods we can use to construct our webserver.
+//the below const is stating that the application is going to be using the express node module.  Express then provides methods we can use to construct a webserver.
 const app = express();
 //.use is needed whenever you want to use a middleware module
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 
-//here's where we mount the router at the specific endpoint, therefore the first parameter is the endpoint then the second parameter is the router
-app.use('/dishes', dishRouter);
+//here's where we mount the router at the specific endpoint, therefore the first parameter is the endpoint(s) then the second parameter is the router
+app.use(['/dishes','/dishes/:dishId'], dishRouter);
+app.use(['/leaders','/leaders/:leaderId'], leaderRouter);
+app.use(['/promotions', '/promotions/promoId'], promoRouter);
 
 
-//The below __dirname method is telling express to look at the static file in the /public file.
+//The below __dirname variable is telling express to look at the static file in the /public file.
 app.use(express.static(__dirname+ '/public'));
 
 //next is used when you need to invoke additional middleware to take care of work on your behalf. In the function below next is an optional parameter.
 app.use((req,res,next) => {
-    // console.log(req.headers);
-
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express server</h1></body></html>')
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -12,6 +12,7 @@ dishRouter.use(bodyParser.json());
 //express router supports the .route method which can take in an endpoint as a parameter
 //The '/' relates to the fact that we're mounting the router of dishes at the '/' in the URI, the mounting takes place in the index.js file.
 dishRouter.route('/')
+//By specifying '/' as a "mount" path, app.use() in index.js will respond to any path that starts with /, which are all of them and regardless of the HTTP verb used
 //by using this approach we are declaring the endpoint at one single location thereby you can chain all of the GET, POST, DELETE methods to that single location (without having to call it with every function handling those methods).
 //previously we had used app.all, but because we are chaining these functions to the route, app is not a const, and is not needed, and neither is the endpoint we had initially had defined 
 .all((req, res, next)=>{
@@ -34,6 +35,25 @@ dishRouter.route('/')
 });
 //note: that when you chain these methods, only the last needs a semi colon.
 
-//The dishRouter chained function will only callable within side this node module unless we allow it to be exported:
 
+
+dishRouter.route('/:dishId')
+.get((req,res,next) => {
+    res.end('Will send details of the dish: '+ req.params.dishId + ' to you!');
+})
+.post((req,res,next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+})
+.put((req,res,next) => {
+    //res.write can be used to add a line to the reply message. (the '\n' adds a new line)
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    //Since this is a PUT operation and if the body contains the JSON string which contains the details of the dish, we can extract the details of the dish because we are using the body parser (using req.body.name as the body (in this example?) contains a name parameter).
+    res.end('Will update the dish '+ req.body.name + ' with details: '+ req.body.description);
+})
+.delete((req,res,next) => {
+    res.end('Deleting dish: '+ req.params.dishId);
+});
+
+//The dishRouter chained function will only callable within side this node module unless we allow it to be exported:
 module.exports = dishRouter;  
